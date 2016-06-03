bfc2082660c53da4b8b04da3fc58c5a79ef173ff
yaroslav-dyn <norman_zp@outlook.com>
2016-06-03 11:30:19 +0300

    source

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var del  = require('del');
+
+
+gulp.task('sass', function () {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/css'));
+});
+
+//build js
+gulp.task('js-build', function(){
+    gulp.src('src/js/*.js')
+    .pipe(gulp.dest('public/js/'))
+});
+//build html with Dependencies
+gulp.task('html-build', function () {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('public/'));
+});
+//build img
+gulp.task('img-build', function () {
+    gulp.src('src/img/**/')
+    .pipe(gulp.dest('public/img/'));
+});
+
+//all build
+gulp.task('build', 
+[
+    'html-build',
+    'js-build',
+    'img-build'
+]);
+
+// Clean public
+gulp.task('build:clean', function(){
+    return del(['public/*']);
+});
+
+
+
+// Static Server + watching css/html files
+gulp.task('serv', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js' ).on('change', browserSync.reload);
+    gulp.watch('src/scss/*.scss',['sass']);
+    gulp.watch('public/**/*.css' ).on('change', browserSync.reload);
+
+});
+
+
+
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Start front-end template projects 
+	
+	## Inside
+		- bootstrap (only grid)
+		- scss
+		- gulp	
+			..*	- build project in 'public'
+			..*	- compile SCSS
+			..*	- server with browsers sync live reload localhost:3000/
+
+	## Structure: 
+		- | lib
+<br>			
+		- | public/
+				..* - fonts/
+				..* - img/
+				..* - js/
+				..* - css/
+				..* - favicon.ico
+				..* - index.html
+<br>
+		- | src / 
+				..* - fonts/
+				..* - img/ 
+				..* - js/
+				..* - scss/
+				..* - html-parts/
+				..* - favicon.ico
+				..* - index.html
+<br>
+		- |	node_modules/
+		- | .gitignore
+<br>
+		- | .gulpfile.js	
+<br>
+		- | .package.json
+<br>		
+		- | .readme.md		
+
+
+
+
+
